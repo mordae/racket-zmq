e144ed3c10a90262c7ac2de09e1c584d8c06497d
Jan Dvořák <mordae@anilinux.org>
2015-08-08 16:06:56 +0200

    Add missing mordae and typed-racket-lib deps
    Signed-off-by: Jan Dvořák <mordae@anilinux.org>

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang setup/infotab
 
 (define collection 'multi)
-(define deps '("base" "scribble-lib" "misc1"))
+(define deps '("base" "scribble-lib" "misc1" "mordae" "typed-racket-lib"))
 (define build-deps '("racket-doc"))
 
 ; vim:set ts=2 sw=2 et:
