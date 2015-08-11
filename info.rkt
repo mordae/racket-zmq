5d936df13adce486ac23c5e921099de10ad9bf61
Jan Dvořák <mordae@anilinux.org>
2015-08-11 12:41:07 +0200

    Add missing typed-racket-doc dependency
    Signed-off-by: Jan Dvořák <mordae@anilinux.org>

--- info.rkt
+++ info.rkt
@@ -2,6 +2,6 @@
 
 (define collection 'multi)
 (define deps '("base" "scribble-lib" "misc1" "mordae" "typed-racket-lib"))
-(define build-deps '("racket-doc" "typed-racket-lib"))
+(define build-deps '("racket-doc" "typed-racket-lib" "typed-racket-doc"))
 
 ; vim:set ts=2 sw=2 et:
